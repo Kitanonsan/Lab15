a84527cb3900f8961b18695e8e35fc771c785795
Non <Kitanon_san@cmu.ac.th>
2022-01-11 17:30:13 +0700

    Lab15_1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for(int i = 1; i <N; i++){
+		T number = d[i];
+		for (int j = 1; j < i+1; j++){
+			T np = d[i-j];
+			if(number > np)
+				d[i-j+1] = np;
+			if (number < np || number == np){
+				d[i-j+1] = number;
+				break;
+			}	
+			if(number > np && (i-j) == 0){
+				d[i-j+1] = np;
+				d[i-j] = number;
+			}
 
+		}
+
+		cout << "Pass " << i << ":" ;
+		for(int i = 0; i < 10; i++) 
+			cout << d[i] << " ";
+		cout << "\n";
+	}
 }
 
 int main(){
